6295852f58b04d960eaa290c988a333f0527e0f6
Nishant Pacharne <nishantpacharne16@gmail.com>
2020-10-03 20:31:41 +0530

    added the requirements.txt file

--- users/urls.py
+++ users/urls.py
@@ -13,6 +13,5 @@ urlpatterns = [
     path('reset-password/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='reset_password'),
     path('reset-password/sent/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('reset-password/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
-
+    path('reset-password/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete')
 ]
